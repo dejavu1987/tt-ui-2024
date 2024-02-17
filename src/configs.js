55088e878a7412bfd5ad7c968cf3b24f3156c9d4
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2024-02-17 18:23:15 +0100

    Read apiUrl from LocalStorage

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,6 @@
 const config = {
-  apiUrl: "http://localhost:8080",
+  // apiUrl: "http://localhost:8080",
+  apiUrl: window.localStorage.getItem("apiUrl") || "http://localhost:8080",
   // apiUrl: process.env.API_URL || 'https://tt-api-eu.herokuapp.com',
 };
 export default config;
