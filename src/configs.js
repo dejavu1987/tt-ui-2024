424e533bf1dc9945bf258cf808e98b5728ba09d1
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2025-04-30 14:19:40 +0200

    Proxy http

--- src/configs.js
+++ src/configs.js
@@ -1,7 +1,7 @@
 const config = {
   // apiUrl: "http://localhost:8080",
   apiUrl: window.localStorage.getItem("proxy")
-    ? `https://${window.localStorage.getItem("proxy")}`
+    ? `http://${window.localStorage.getItem("proxy")}`
     : "http://localhost:8080",
   // apiUrl: process.env.API_URL || 'https://tt-api-eu.herokuapp.com',
 };
